030dd1e6e7ec52cbb6242fd3ee7f1954f7de9529
Lay Lo <llo@adaptivepulse.com>
2022-12-12 23:23:34 -0500

    Done resource

--- src/main/java/com/chillsoba/restapi/resource/ChairResource.java
+++ src/main/java/com/chillsoba/restapi/resource/ChairResource.java
@@ -1,13 +1,39 @@
 package com.chillsoba.restapi.resource;
 
+import com.chillsoba.restapi.object.Chair;
 import com.chillsoba.restapi.object.ChairRepo;
 import com.chillsoba.restapi.service.ChairService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ChairResource {
 
     ChairService service;
+    public ChairResource(ChairService service) {
+        this.service = service;
+    }
+    @GetMapping(value = "/chairs")
+    public List<Chair> getChairs() {return this.service.getChairs();}
 
+    @GetMapping(value = "/chairs/{id}")
+    public Chair getChair(Integer id){
+        return this.service.getChair(id);
+    }
 
+    @PostMapping(value = "/chairs")
+    public Chair postChair(@RequestBody Chair chair ){
+        return this.service.postChair(chair);
+    }
+
+    @PutMapping(value = "/cloths/{id}", consumes = "application/json")
+    public Chair update(@PathVariable Integer id, @RequestBody Chair chair){
+        return this.service.putChair(id, chair);
+    }
+
+    @DeleteMapping(value = "/cloths/{id}")
+    public void deleteChair(@PathVariable Integer id){
+        this.service.deleteChair(id);
+    }
 }
--- src/main/java/com/chillsoba/restapi/service/ChairService.java
+++ src/main/java/com/chillsoba/restapi/service/ChairService.java
@@ -1,5 +1,6 @@
 package com.chillsoba.restapi.service;
 
+import com.chillsoba.restapi.object.Chair;
 import com.chillsoba.restapi.object.ChairRepo;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,28 @@ public class ChairService {
 
     ChairRepo repo;
 
-    public ChairService(ChairRepo repo) {
-        this.repo = repo;
+    public ChairService(ChairRepo repo) {this.repo = repo;}
+
+    public List<Chair> getChairs(){return this.repo.findAll();}
+
+    public Chair getChair(Integer id) {return this.repo.findById(id).get();}
+
+    public Chair postChair(Chair chair) {return this.repo.save(chair);}
+
+    public Chair putChair(Integer id, Chair chair){
+        Optional<Chair> object = this.repo.findById(id);
+
+        if(object.isPresent()){
+            object.get().setColour(chair.getColour());
+            object.get().setMaterial(chair.getMaterial());
+            object.get().setSize(chair.getSize());
+            return this.repo.save(object.get());
+        }
+        throw new RuntimeException();
     }
+
+    public void deleteChair(Integer id) {this.repo.deleteById(id);}
+
+
 
 }
